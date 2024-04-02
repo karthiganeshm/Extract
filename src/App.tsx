f0906175f4d649743afc01d04dbfc758777c02f8
karthi06 <karthiganesh.m@6solve.com>
2024-04-02 15:03:24 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ExtractSelectWithTag}  from './components/ExtractSelectWithTag';
+function App() {
+  return (
+    <div >
+    <ExtractSelectWithTag/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExtractSelectWithTag.tsx
@@ -0,0 +1,108 @@
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { Select, Space, Tag, Checkbox } from "antd";
+import type { SelectProps } from "antd";
+import Styles from "../mystyles.module.css";
+import { originalDataHeaders } from "../utils/data";
+
+// options = [{ label: 'Gold', value: 'Gold' }, { label: 'Silver', value: 'Silver' }, { label: 'Platinum', value: 'Platinum' }, { label: 'Diamond', value: 'Diamond' }];
+
+export const ExtractSelectWithTag: FunctionComponent = () => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [selectionCount, setSelectionCount] = useState<number>(0);
+  const [options, setOptions] = useState<SelectProps["options"]>([]);
+  useEffect(() => {
+    const flattenedFields: string[] = originalDataHeaders.reduce(
+      (accumulators: string[], currentValue) => {
+        const combinedFields = accumulators.concat(currentValue.fields);
+        return Array.from(new Set(combinedFields));
+      },
+      []
+    );
+
+    console.log("flattenedFields", flattenedFields);
+
+    const updatedOptions = flattenedFields.map((item) => ({
+      label: item,
+      value: item,
+    }));
+
+    console.log("options", updatedOptions);
+
+    setOptions(updatedOptions);
+  }, []);
+
+  const forMap = (tag: string) => (
+    <>
+      <span key={tag} style={{ display: "inline-block" }}>
+        <Tag
+          closable
+          onClose={(e) => {
+            e.preventDefault();
+            handleClose(tag);
+          }}
+        >
+          {tag}
+        </Tag>
+      </span>
+    </>
+  );
+  const handleclearall = () => {
+    setTags([]);
+    setSelectionCount(0);
+  };
+  const tagChild = tags.map(forMap);
+
+  const handleChange = (value: string[]) => {
+    if (!tags.includes(`${value}`)) {
+      console.log(`selected ${value}`);
+      setTags((prevTags) => [...prevTags, ...value]);
+      setSelectionCount(tags.length + 1);
+      console.log(tags, "tags");
+    }
+  };
+
+  const handleClose = (removedTag: string) => {
+    const newTags = tags.filter((tag) => tag !== removedTag);
+    setTags([...newTags]);
+    setSelectionCount(newTags.length);
+  };
+
+  return (
+    <div style={{ width: "40%" }}>
+      <Space style={{ width: "100%", margin: 10 }} direction="vertical">
+        <Select
+          mode="multiple"
+          allowClear
+          style={{ width: "100%" }}
+          placeholder={
+            selectionCount ? `${selectionCount} Selected` : "Please Select"
+          }
+          onChange={handleChange}
+          value={null}
+          options={options}
+          optionRender={(option) => (
+            <Space className={Styles.selectorOption}>
+              <span>
+                {" "}
+                <Checkbox checked={tags.includes(`${option.label}`)} />
+              </span>
+              <span>{option.label}</span>
+            </Space>
+          )}
+          size="large"
+          maxTagCount={2}
+          disabled={tags.length >= 2}
+        />
+        <div>
+          <span className={Styles.tag}> {tagChild} </span>{" "}
+          {tags.length > 0 && (
+            <span onClick={handleclearall} className={Styles.clearTag}>
+              {" "}
+              Clear all
+            </span>
+          )}
+        </div>
+      </Space>
+    </div>
+  );
+};
